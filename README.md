ab23baab73d8dced044f450f3e0d98ca38ac086a
Gary King <cyberdad31t@gmail.com>
2022-07-27 14:43:34 -0700

    adds challenge 3

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Tabel of Contents
 - [Ops challeng 02](helloworld.sh)
+- [Ops challenge 3](login.sh)
---
+++ login.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# varible
+last="last"
+
+#function
+show_logins (){
+	$last
+}
+
+#invoke  the  function
+show_logins
+
