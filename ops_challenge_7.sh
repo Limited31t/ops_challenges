c66d2e3224b7b6f83e18f66d23bc85192aee0ef1
Gary King <limited31t@yahoo.com>
2022-08-02 21:28:04 -0700

    challenge 7 complete '

--- ops_challenge_7.sh
+++ ops_challenge_7.sh
@@ -13,18 +13,3 @@ echo "Display adapter"
 sudo lshw | grep -A 12 '*-display' 
 echo "network adapter"
 sudo lshw | grep -A 14 '*-display'
-l#!/bin/bash
-       
-# Author:Gary King                    
-# Date of latest revision:08/02/2022 
-# Purpose: System Information
-    echo "computer name"
-cat /proc/sys/kernel/hostname
-    echo "cpu information"
-sudo lshw | grep -A 7 '*-cpu' 
-    echo "RAM"
-sudo lshw | grep -A 3 '*-memory' 
-    echo "Display adapter"
-sudo lshw | grep -A 12 '*-display' 
-    echo "network adapter"
-sudo lshw | grep -A 14 '*-display'
