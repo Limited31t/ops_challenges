c8452ae3e686364a021bfc4761dd2f7fc1a71ed6
Gary King <109040766+Limited31t@users.noreply.github.com>
2022-08-11 16:30:54 -0700

    Create ops_challenge_14.ps1

---
+++ ops_challenge_14.ps1
@@ -0,0 +1,30 @@
+Gary King
+08/11/2022
+Class 14 Ops Challenge
+What is this repo all about?
+My Ops Challenge repo page is where we have been putting all of our scripts that have been written during our Seattle-Ops-201d5 class for anybody to go see 
+the work we have been performing during the 201 class challenges. 
+
+Why are you learning these skills?
+I am learning these new skills in the effort to begin a new career in the IT / CyberOps world to hopefully enter into a new career.
+
+What fundamental concepts did you learn in Ops 201? T
+The fundamentals that I am learning in the 201 class are how the computer can be operated remotely, I am learning about how to build virtual boxes and cloning 
+them so that I don't have to rebuild a box every time I need a new one,we have learned about script writing which I am still needing to brush up on because I 
+am still having issues remembering what needs to go where so I will be brushing up on this now that we are having a little more free time so I will be ready 
+for our exame.
+
+What was your favorite Ops Challenge assignment and why? 
+If I had to pick one it would probably be Ops Challenge 13 just because we was able to pick a website that I wanted to script about so I was able to run the script 
+with flowracing.com and this is a site that I can watch all kind of racing on from dirt track racking to drag strip racing.
+
+How can these examples of shell scripting be applied to the workplace? 
+These script writings can help expedite running multiple searches with just one string, so instead of writing 5 separate scripts and then running them and then 
+saving them to a file you run them all as one script and it can all be done once so that will save time for you to work on more things during the work day.
+
+What scripts would you continue to improve if you had the time? 
+I would say that I will be working harder on the Loops, Arrays and then also learning the commands better and where they need to be when writing the scripts.
+
+What aspect of your shell scripting skillset do you hope to improve next? 
+I would say the skillset that I need to work on is probably in general just learning the different language type and what commands go with which set because I am 
+still trying to get those somewhat straight in my head.
