dc58b6c3df41e1f5a61e9cfa64ac2afce5fb6ef7
Gary King <limited31t@yahoo.com>
2022-09-27 14:53:32 -0700

    completed conditionals challenge

---
+++ conditionals.py
@@ -0,0 +1,34 @@
+#!/bin/python
+
+# script ops challenge 10
+# Author Gary King
+# Last edited date 09/27/2022
+
+#variables
+a = 42
+b = 1001
+
+# Main
+# Equals: a == b
+# Strictly "equals each other"
+if a == b:
+    print("A is is strictly equals to B")
+elif a != b:
+ # Not Equals: a != b   
+        print("A is not strictly equal to B")
+
+# Less than: a < b
+if a < b:
+    print("A is less than B")
+
+elif a > b:
+    print("A is greater than to B")
+    
+
+# Less than or equal to: a <= b
+if a <= b:
+    print ("A is less than or equal to B")
+# Greater than or equal to: a >= b
+elif a >= b:
+    print("A is greater than or equal to B")
+
